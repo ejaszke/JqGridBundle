36122310faeefe1798638ee15ecdc72ac40379c9
Edward Jaszke <edward.jaszke@gmail.com>
2017-08-27 09:02:48 +0200

    fix refactored classes

--- Twig/JqGridExtension.php
+++ Twig/JqGridExtension.php
@@ -50,19 +50,19 @@ class JqGridExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-                'jqgrid' => new \Twig_SimpleFunction($this, 'renderGrid',
+                'jqgrid' => new \Twig_SimpleFunction('renderGrid',[$this, 'renderGrid'],
                         array(
                             'is_safe' => array(
                                 'html'
                             )
                         )),
-                'jqgrid_js' => new \Twig_SimpleFunction($this, 'renderGridJs',
+                'jqgrid_js' => new \Twig_SimpleFunction('renderGridJs',[$this, 'renderGridJs'],
                         array(
                             'is_safe' => array(
                                 'html'
                             )
                         )),
-                'jqgrid_html' => new \Twig_SimpleFunction($this, 'renderGridHtml',
+                'jqgrid_html' => new \Twig_SimpleFunction('renderGridHtml', [$this, 'renderGridHtml'],
                         array(
                             'is_safe' => array(
                                 'html'
